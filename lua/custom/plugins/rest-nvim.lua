beeb414c4e3836a0663a921aee163149aa955ff0
David Fisk <dfisk@yahooinc.com>
2024-11-05 15:28:02 -0800

    rest support

--- lua/custom/plugins/rest-nvim.lua
+++ lua/custom/plugins/rest-nvim.lua
@@ -2,5 +2,11 @@ return {
   'rest-nvim/rest.nvim',
   config = function()
     -- vim.g.rest_nvim = {}
+    vim.keymap.set('n', '<leader>rr', ':Rest run<CR>', { desc = '[R]est [R]un' })
+    vim.keymap.set('n', '<leader>rl', ':Rest logs<CR>', { desc = '[R]est [L]ogs' })
+    vim.keymap.set('n', '<leader>rc', ':Rest cookies<CR>', { desc = 'Edit [R]est [C]ookies' })
+    require('telescope').load_extension 'rest'
+    --
+    vim.keymap.set('n', '<leader>se', require('telescope').extensions.rest.select_env, { desc = '[S]earch Rest [E]nvs' })
   end,
 }
