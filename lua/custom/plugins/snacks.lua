a11d2ddf52b298ed28d91d1bea25ffadb26186a7
David <mail@davidfisk.com>
2025-01-05 20:52:24 -0800

    adding new bg for nvim db

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -13,7 +13,7 @@ return {
       sections = {
         {
           section = 'terminal',
-          cmd = 'chafa ~/.config/nvim/waterfall.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
+          cmd = 'chafa ~/.config/nvim/landscape2.jpg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
           height = 17,
           padding = 1,
         },
