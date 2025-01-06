203b0cef5dc1f699bf2c90b6b5c963a8ad1f362f
David <mail@davidfisk.com>
2025-01-05 19:35:30 -0800

    adding dashboard image

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -8,7 +8,22 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      sections = {
+        {
+          section = 'terminal',
+          cmd = 'chafa ~/.config/nvim/waterfall.png --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1',
+          height = 17,
+          padding = 1,
+        },
+        {
+          pane = 2,
+          { section = 'keys', gap = 1, padding = 1 },
+          { section = 'startup' },
+        },
+      },
+    },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
