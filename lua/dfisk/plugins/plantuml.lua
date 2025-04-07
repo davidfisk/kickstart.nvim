6d168ed74f79c903fa2086dc3d06a1bfcd469275
David <mail@davidfisk.com>
2025-04-06 18:12:59 -0700

    little updates

--- lua/dfisk/plugins/plantuml.lua
+++ lua/dfisk/plugins/plantuml.lua
@@ -1,8 +1,11 @@
 -- https://jekhokie.github.io/linux/vim/plantuml/2020/07/25/plantuml-using-vim.html
 
-vim.keymap.set('n', '<leader>dp', ':PlantumlOpen<CR>', { desc = '[D]ocument [P]lantuml' })
 return {
+  ft = { 'plantuml', 'puml' },
   'aklt/plantuml-syntax',
   'tyru/open-browser.vim',
   'weirongxu/plantuml-previewer.vim',
+  config = function()
+    vim.keymap.set('n', '<leader>dp', ':PlantumlOpen<CR>', { desc = '[D]ocument [P]lantuml' })
+  end,
 }
