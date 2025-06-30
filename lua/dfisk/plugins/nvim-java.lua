d6099cd53842e57a5b720ed8c130db86fd671d13
dfisk-y <dfisk@yahooinc.com>
2025-06-30 14:47:08 -0700

    updating java

--- lua/dfisk/plugins/nvim-java.lua
+++ lua/dfisk/plugins/nvim-java.lua
@@ -1,7 +1,11 @@
+-- https://github.com/nvim-java/nvim-java
+-- https://github.com/nvim-java/nvim-java/issues/374
+-- https://github.com/nvim-java/nvim-java/issues/384
 return {
   'nvim-java/nvim-java',
   priority = 100,
   config = function()
+    require('java').setup();
     require('lspconfig').jdtls.setup = {
       java = {
         format = {
